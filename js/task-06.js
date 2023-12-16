cd18d0ca2c89390611e636ec986eb9db0e9ea00a
adelife <142346901+adelife@users.noreply.github.com>
2023-12-16 20:32:01 +0200

    .

--- js/task-06.js
+++ js/task-06.js
@@ -1,14 +1,28 @@
-const input = document.querySelector('input');
-const btnCreate = document.querySelector('[data-create]');
-const btnDestroy = document.querySelector('[data-destroy]');
-console.log(btnDestroy);
-
-function createBoxes(amount) {}
-
-function createBoxes(amount) {}
-function destroyBoxes() {}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+const formInput = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+function destroyEl() {
+  boxes.textContent = '';
+}
+function createBoxes(amount) {
+  destroyEl();
+  for (let i = 0; i < amount; i++) {
+    let size = 30 + 10 * i;
+    const divBox = document.createElement('div');
+    divBox.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    boxes.insertAdjacentElement('beforeend', divBox);
+  }
+  formInput.value = '';
+}
+btnCreate.addEventListener('click', () => {
+  if (1 <= formInput.value <= 100) {
+    createBoxes(formInput.value);
+  }
+});
+btnDestroy.addEventListener('click', destroyEl);
